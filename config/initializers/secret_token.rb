f89ee4380f29ddb89b455df97e90701882cbe7eb
martafonda <martafondapascual@gmail.com>
2013-12-06 16:32:55 +0000

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Makeyourtrip::Application.config.secret_key_base = '4ed01819e8b6d01308499a561089189a7338092b3b7e0780a215fd23b7c865c50da3920d39150609f5045f1e1709088909f2ee393bdd0c2fcea111168d328442'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Makeyourtrip::Application.config.session_store :cookie_store, key: '_makeyourtrip_session'
