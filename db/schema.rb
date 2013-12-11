5c03355a21b2d55d38a767a707981ceec1c2a07d
martafonda <martafondapascual@gmail.com>
2013-12-11 11:42:50 +0000

    model city created. Migration done

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131209121313) do
+ActiveRecord::Schema.define(version: 20131211100644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: true do |t|
+    t.string   "name"
+    t.date     "arrival"
+    t.date     "departure"
+    t.text     "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
