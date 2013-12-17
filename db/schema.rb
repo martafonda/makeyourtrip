f1a4ff3ce9a0d8fd25e299a50ab99bad2c97aac2
martafonda <martafondapascual@gmail.com>
2013-12-17 10:01:35 +0000

    Places schema modified. Longitude and Latitude added in Places

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131215161929) do
+ActiveRecord::Schema.define(version: 20131217095604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20131215161929) do
     t.integer  "city_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "places", ["city_id"], name: "index_places_on_city_id", using: :btree
